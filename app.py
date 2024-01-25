c476ac297701b61fed85ed8006ec16dbb63c8058
Gustavo Xisto Notebook <curveloxisto@gmail.com>
2024-01-25 10:15:40 -0300

    Feat: Rotas de autenticação

---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "minha_chave_123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+login_manager = LoginManager()
+db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+CORS(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=True)
+
+# Autenticação
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(username=data.get("username")).first()
+    if user and data.get('password') == user.password:
+        login_user(user)
+        return jsonify({'message': "Loggend in successfuly"}), 200
+    
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
+
+@app.route('/logout', methods=['POST'])     
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': "Logout successfuly"}), 200
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+@app.route('/api/products/add', methods=["POST"])
+@login_required
+def add_products():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data['name'], price=data['price'], description=data.get('description', ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({'message': "Product added successfuly"}), 200
+    return jsonify({'message': "Invalid product data"}), 400
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
+def delete_products(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({'message': "Product deleted successfuly"}), 200
+    return jsonify({'message': "Product not found"}), 404
+
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_products_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description,
+        })
+    return jsonify({'message': "Product not found"}), 404
+
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+@login_required
+def update_products(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({'message': "Product not found"}), 404
+
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+
+    if 'description' in data:
+        product.description = data['description']
+    
+    db.session.commit()
+
+    return jsonify({'message': "Product update successfuly"}), 200
+
+@app.route('/api/products', methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+        }
+        product_list.append(product_data)
+        
+    return jsonify(product_list), 200
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello word'
+
+if __name__ == '__main__':
+    app.run(debug=True)
